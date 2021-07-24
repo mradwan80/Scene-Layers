2114550ad30c9f7a62126ac93ba659bbe2115bfa
msalah80 <msalah80@gmail.com>
2021-07-24 18:46:45 +0200

    cuda test code removed

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,6 @@
 #include<thrust/device_vector.h>
 #include<thrust/device_ptr.h>
 
-void func(int size, int* a1, int* a2, int* a3);
-void FillWithValue(int* arr, int size, int val);
-
 struct Position
 {
 	float x,y,z;
@@ -135,36 +132,6 @@ int main()
 	glEnable(GL_PROGRAM_POINT_SIZE);
 	glViewport(0, 0, GlobalW, GlobalH);
 	glEnable(GL_DEPTH_TEST);
-
-	
-	/////////
-	//testing cuda
-	////////
-	/*
-	int size=1000;
-	int *arr1, *arr2, *arr3;
-	
-	cudaMalloc((void**)&arr1, size * sizeof(int));
-	FillWithValue(arr1,size,1);
-
-	cudaMalloc((void**)&arr2, size * sizeof(int));
-	FillWithValue(arr2,size,2);
-
-	cudaMalloc((void**)&arr3, size * sizeof(int));
-
-	int* harr = new int [size];
-	cudaMemcpy(harr,arr1,size*sizeof(int),cudaMemcpyDeviceToHost);
-	fprintf(stdout, "%d\n",harr[0]);
-
-
-	func(size, arr1, arr2, arr3);
-	cudaError_t errc = cudaGetLastError();
-	if (cudaSuccess != errc)
-		fprintf(stderr, "Cuda error: %s.\n", cudaGetErrorString(errc));
-	*/
-	//////////
-	//////////
-	//////////
 
 	//render loop//
 	while (!glfwWindowShouldClose(myWindow))
